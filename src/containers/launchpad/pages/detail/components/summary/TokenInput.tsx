2c632fa593f23c1d0797ed3287c1937ea18fd252
leon-do <leondo123@gmail.com>
2025-01-29 10:11:56 -0500

    fix build

--- src/containers/launchpad/pages/detail/components/summary/TokenInput.tsx
+++ src/containers/launchpad/pages/detail/components/summary/TokenInput.tsx
@@ -33,7 +33,7 @@ export function TokenInput({ icon, symbol, balance, value, onChange }: Props) {
       <Spacing height={8} />
 
       { balance == 0 &&
-        <Txt>Your PEPU balance is too low, <a href={process.env.NEXT_PUBLIC_DEX_URL} target="_blank" style={{ textDecoration: "underline" }}>buy here</a> </Txt>
+        <Txt>Your PEPU balance is too low, <a href={process.env.NEXT_PUBLIC_DEX_URL} target="_blank" style={{ textDecoration: "underline" }} rel="noreferrer">buy here</a> </Txt>
       }
 
       <Spacing height={8} />
