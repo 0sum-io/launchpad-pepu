8a38d9273410c097d7ffa9089711b10290eef750
leon-do <leondo123@gmail.com>
2025-02-03 10:53:15 -0500

    update img

--- src/remotes/uploadImage.ts
+++ src/remotes/uploadImage.ts
@@ -3,7 +3,7 @@ export async function uploadImage(file: File) {
   const formData = new FormData();
   formData.append("image", file);
   const request = await fetch(
-    `https://api.imgbb.com/1/upload?key=411f454fe6994c276ac5cb714518decf`,
+    `https://api.imgbb.com/1/upload?key=2ae89f0085a842e1ddb6261602cdc71e`,
     {
       method: "POST",
       body: formData,
